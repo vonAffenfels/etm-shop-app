ed7eaaa2709acea66719b3dcec072c58f424ecfe
Julian Lindner <julian.lindner@vonaffenfels.de>
2021-09-21 13:54:56 +0200

    update

---
+++ server/handlers/mutations/getProduct.js
@@ -0,0 +1,33 @@
+import gql from "graphql-tag";
+
+const getProduct = async (client, id) => {
+    const res = await client.query({
+        query: gql`
+            query($id:ID!) {
+                product(id:$id) {
+                    title
+                    description
+                    metafields(first: 1, namespace: "Download") {
+                        edges {
+                            node {
+                                id
+                                key
+                                namespace
+                                createdAt
+                                description
+                                value
+                            }
+                        }
+                    }
+                }
+            }
+        `,
+        variables: {
+            id: id
+        }
+    });
+
+    return res;
+};
+
+export default getProduct;
