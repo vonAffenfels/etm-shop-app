d8f61130348a8685506adf4a7932f0e415f0b6c7
Julian Lindner <julian.lindner@vonaffenfels.de>
2021-09-29 11:33:30 +0200

    update

--- server/server.js
+++ server/server.js
@@ -150,12 +150,7 @@ app.prepare().then(async () => {
             console.log("downloadDateFields", downloadDateFields)
             if (downloadDateFields.length) {
                 let dateTime = new Date(downloadDateFields[0].value).getTime();
-                console.log("dateTime", dateTime)
-                console.log("new Date", (new Date().getTime()))
-                console.log(new Date().getTime() < dateTime)
-                return ctx.body = {
-                    error: "releasedate not reached"
-                }
+
                 if (new Date().getTime() < dateTime) {
                     return ctx.body = {
                         error: "releasedate not reached"
