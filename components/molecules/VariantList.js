74a1bc8155c1168daeb4d391c7802c3d30002036
Julian Lindner <julian.lindner@vonaffenfels.de>
2021-10-18 15:06:03 +0200

    update

--- components/molecules/VariantList.js
+++ components/molecules/VariantList.js
@@ -24,12 +24,23 @@ const VariantList = ({existingProduct}) => {
     }
 
     return (
-        <ResourceList
-            showHeader
-            resourceName={{singular: "Variante", plural: "Varianten"}}
-            items={variants.edges}
-            renderItem={(item) => <VariantItem item={item} update={update.bind(this)} />}
-        />
+        <div className="Polaris-ResourceList__ResourceListWrapper">
+            <div className="Polaris-ResourceList__HeaderOuterWrapper">
+                <div>
+                    <div style={{paddingBottom: "0px"}}></div>
+                    <div>
+                        <div className="Polaris-ResourceList__HeaderWrapper">
+                            <div className="Polaris-ResourceList__HeaderContentWrapper">
+                                <div className="Polaris-ResourceList__HeaderTitleWrapper">{variants.edges.length} Varianten</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <ul className="Polaris-ResourceList">
+                {variants.edges.map((item) => <VariantItem item={item} update={update.bind(this)} />)}
+            </ul>
+        </div>
     );
 };
 
